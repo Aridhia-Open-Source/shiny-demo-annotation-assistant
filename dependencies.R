68923e99c4f356fe909d15850dd1950ff26991ff
hpeaker <harry_peaker@hotmail.com>
2021-03-10 14:09:27 +0000

    only install if required

--- dependencies.R
+++ dependencies.R
@@ -1,9 +1,15 @@
-install.packages("shiny")
-install.packages("rJava")
-install.packages("qdap")
-install.packages("tm")
-install.packages("stringdist")
-install.packages("RWeka")
-install.packages("stringr")
-install.packages("BiocManager")
+package_install <- function(x, ...) {
+  for (i in x) {
+    # Check if package is installed
+    if (!require(i, character.only = TRUE)){
+      # If the package could not be loaded then install it
+      install.packages(i, ...)
+    }
+  }
+}
+
+packages <- c("shiny", "rJava", "qdap", "tm", "stringdist", "RWeka", "stringr", "BiocManager")
+
+package_install(packages)
+
 BiocManager::install("IRanges")
